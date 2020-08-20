b8699e50c6c07cbaa2c8b674cc90ab17898d4466
Firuz Khalimov <fkhalimov22@gmail.com>
2020-08-20 15:09:01 -0400

    Update

---
+++ src/test/java/Day02/Zip_code_test.java
@@ -0,0 +1,55 @@
+package Day02;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Zip_code_test {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI="http://api.zippopotam.us";
+        basePath="/us";
+
+    }
+
+    @DisplayName("Zip to City Test")
+    @Test
+    public void testZipCode(){
+
+       given()
+               .log().all()
+               .accept(ContentType.JSON).
+               pathParam("zipcode",19114).
+
+       when()
+               .get("/{zipcode}").
+
+       then()
+
+               .log().all()
+               .statusCode(200)
+               .contentType(ContentType.JSON)
+
+               .contentType(ContentType.JSON)
+               .body("country", is("United States"))
+               .body("'post code'",is("19114"))
+               .body("'country abbreviation'",is("US"))
+               .body("places[0].'place name'",is("Philadelphia"))
+               .body("places[0].longitude",is("-74.999"))
+               .body("places[0].state",is("Pennsylvania"))
+               .body("places[0].'state abbreviation'",is("PA"))
+               .body("places[0].latitude",is("40.0634"));
+
+
+
+    }
+
+
+
+}
